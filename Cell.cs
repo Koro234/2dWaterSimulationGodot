30d99e2faa89d372aa874207cff249837b8600c6
Koro234 <tra.ne555222@yandex.ru>
2021-05-26 17:29:24 +0400

    first test

--- Cell.cs
+++ Cell.cs
@@ -1,12 +1,80 @@
 using Godot;
 using System;
 
-public class Cell : Node
+public interface ICellState
 {
+    void Calculation(Cell cell);
+}
 
-    public override void _Ready()
+public class Cell
+{
+    public int x {get; set;}
+    public int y {get; set;}
+    public int TileNum {get; set;} = 0;
+    public Cell[] neighbours {get; set;}
+    public ICellState State {get; set;}
+    public Cell(ICellState cs)
     {
-        
+        State = cs;
+    }
+    public void Calculation()
+    {
+        State.Calculation(this);
     }
 
+}
+public class WaterCell : ICellState
+{
+    public float maxFluidValue = 1.0f;
+    public float minFluidValue = 0.005f;
+    public float fluidQuantity = 0.0f;
+    public float maxFlowValue = 4.0f;
+    public float minFlowValue = 0.005f;
+    public float flowSpeed = 1.0f;
+    public void Calculation(Cell cell)
+    {
+        cell.TileNum = (int)(fluidQuantity*10/8) + 1;
+        if (cell.neighbours == null)
+        {
+            return;
+        }
+        if (fluidQuantity == 0.0f)
+        {
+            return;
+        }
+        if (fluidQuantity < minFluidValue)
+        {
+            fluidQuantity = 0.0f;
+            return;
+        }
+        foreach (var neighbour in cell.neighbours)
+        {
+            if (neighbour.State is WaterCell)
+            {
+                var n = neighbour.State as WaterCell;
+                float flow = (fluidQuantity - n.fluidQuantity)/4;
+                if (flow > minFlowValue)
+                {
+                    flow *= flowSpeed;
+                }
+                flow = Math.Max(flow, 0.0f);
+                if (flow > Math.Min(maxFlowValue, fluidQuantity))
+                {
+                    flow = Math.Min(maxFlowValue, fluidQuantity);
+                }
+                if (flow != 0.0f)
+                {
+                    n.fluidQuantity += flow;
+                    fluidQuantity -= flow;
+                }
+            }
+        }
+    }
+}
+public class EmptyCell : ICellState
+{
+    public void Calculation(Cell cell)
+    {
+        return;
+    }
 }
--- Fluid_sim.cs
+++ Fluid_sim.cs
@@ -1,13 +1,13 @@
 using Godot;
 using System;
 
-public class Fluid_sim : Godot.Node
+public class Fluid_sim : Godot.Node2D
 {
     private TileMap _tileMap;
     [Export]
     public int gridX = 128;
     [Export]
-    public int gridY = 128;
+    public int gridY = 75;
     [Export]
     private float timeTicRate = 0.08f;
     private float timePassed = 0.0f;
@@ -15,8 +15,87 @@ public class Fluid_sim : Godot.Node
 
     public override void _Ready()
     {
-        
+        _tileMap = GetNode<TileMap>("TileMap");
+        _cellBuffer = new Cell[gridX, gridY];
+        CellCreater();
+        SetNeighbors();
     }
+    private void Inject()
+    {
+        if (Input.IsMouseButtonPressed(1))
+        {
+            var waterCell = (Cell)_cellBuffer[(int)(GetGlobalMousePosition().x/8), (int)(GetGlobalMousePosition().y/8)];
+            var wc = waterCell.State as WaterCell; 
+            wc.fluidQuantity += 10f;
+        }
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Inject();
+        timePassed += delta;
+        if (timePassed > timeTicRate)
+        {
+            UpdateSimulation();
+            UpdateSimulation();
+            UpdateSimulation();
+            UpdateSimulation();
+            timePassed = 0f;
+        }
+    }
+
+    private void UpdateSimulation()
+    {
+        for (int i = 0; i < gridX; i++)
+        {
+            for (int j = 0; j < gridY; j++)
+            {
+                _cellBuffer[i, j].Calculation();
+                UpdateMap(_cellBuffer[i, j]);
+            }
+        }
+    }
+    private void UpdateMap(Cell c)
+    {
+        _tileMap.SetCell(c.x, c.y, c.TileNum);
+    }
+    private void CellCreater()
+    {
+        for (int i = 0; i < gridX; i++)
+        {
+            for (int j = 0; j < gridY; j++)
+            {
+                Cell c = new Cell(new WaterCell());
+                c.x = i;
+                c.y = j;
+                _cellBuffer[i,j] = c;
+            }
+        }
+    }
+    private void SetNeighbors()
+    {
+        for (int i = 0; i < gridX; i++)
+        {
+            for (int j = 0; j < gridY; j++)
+            {
+                if (i > 0 && i < (gridX - 1) && j > 0 && j < (gridY-1))
+                {
+                    _cellBuffer[i, j].neighbours = PackagingNeighbours(i, j);
+                }
+            }
+        }
+    }
+    private Cell[] PackagingNeighbours(int x, int y)
+    {
+        Cell[] package = new Cell[4];
+        package[0] = _cellBuffer[x, y - 1];
+        package[1] = _cellBuffer[x + 1, y];
+        package[2] = _cellBuffer[x, y + 1];
+        package[3] = _cellBuffer[x - 1, y];
+
+        return package;
+    } 
+
 
 
 }
